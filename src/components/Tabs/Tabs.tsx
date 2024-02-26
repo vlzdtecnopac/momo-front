7c4647800a4f3c6db871c1e7f4d2b3d1f370b47e
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-02-26 14:12:32 -0500

    Update App

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,74 +1,33 @@
 import "./Tabs.scss";
 
 import flagIcon from "../../assets/icons/flag.svg";
-import bellIcon from "../../assets/icons/bell.svg";
-import clockIcon from "../../assets/icons/clock.svg";
 import OrderCard from "../OrderCard/OrderCard";
+
 function Tabs() {
   return (
-    <div className="tabs">
-      <input
-        type="radio"
-        id="tab1"
-        name="tab-control"
-        defaultChecked
-      />
-      <input
-        type="radio"
-        id="tab2"
-        name="tab-control"
-      />
-      <input
-        type="radio"
-        id="tab3"
-        name="tab-control"
-      />
-
-      <ul>
-        <li title="completed">
-          <label
-            htmlFor="tab1"
-            role="button"
-          >
-            <img
-              src={flagIcon}
-              alt="flagIcon"
-            />
-            <br />
-            <span>Pedidos Completados</span>
-          </label>
-        </li>
-        <li title="proccess">
-          <label
-            htmlFor="tab2"
-            role="button"
-          >
-            <img
-              src={bellIcon}
-              alt="bellIcon"
-            />
-            <br />
-            <span>Pedidos en Proceso</span>
-          </label>
-        </li>
-        <li title="incoming">
-          <label
-            htmlFor="tab3"
-            role="button"
-          >
-            <img
-              src={clockIcon}
-              alt="clockICon"
-            />
-            <br />
-            <span>Pedidos Entrantes</span>
-          </label>
-        </li>{" "}
-      </ul>
-      <div className="content-tab">
-          <OrderCard />
+    <>
+      <div className="tabs">
+        <ul>
+          <li>
+            <div className="tab-button">
+              <img src={flagIcon} alt="flagIcon" />
+              <br />
+              <span>Pedidos Completados</span>
+            </div>
+          </li>
+          <li className="active">
+            <div className="tab-button active">
+              <img src={flagIcon} alt="flagIcon" />
+              <br />
+              <span>Pedidos Completados</span>
+            </div>
+          </li>
+        </ul>
       </div>
-    </div>
+      <div className="content-tab">
+        <OrderCard />
+      </div>
+    </>
   );
 }
 export default Tabs;

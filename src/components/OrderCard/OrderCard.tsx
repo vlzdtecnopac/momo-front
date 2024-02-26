72c3d35bdb53c5393185000dfc6b2d86906322ce
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-02-26 18:28:40 -0500

    Update App

--- src/components/OrderCard/OrderCard.tsx
+++ src/components/OrderCard/OrderCard.tsx
@@ -2,11 +2,10 @@ import CompleteOrderButton from "../CompleteOrderButton/CompleteOrderButton";
 import TakeoutBadge from "../TakeoutBadge/TakeoutBadge";
 import "./OrderCard.scss";
 
-export enum State{
+export enum State {
   initial = "initial",
   pending = "pending",
-  completed = "completed"
-
+  completed = "completed",
 }
 
 interface Props {
@@ -14,43 +13,45 @@ interface Props {
 }
 
 const OrderCard: React.FC<Props> = (props) => {
+  const randomNum = Math.random();
+  const id_input = "id_" + randomNum.toString(16).substr(2);
   return (
     <div className={`state_${props.state}`}>
-    <div className="order-card">
-      <div className="client_info">
-        <div className="left">
-          <h2>Laura</h2>
-          <TakeoutBadge />
+      <div className="order-card">
+        <div className="client_info">
+          <div className="left">
+            <h2>Laura</h2>
+            <TakeoutBadge />
+          </div>
+          <div className="right">
+            <span>00:00:00</span>
+            <p>ID #12356</p>
+          </div>
         </div>
-        <div className="right">
-          <span>00:00:00</span>
-          <p>ID #12356</p>
+        <div className="order_info">
+          <div className="item_name">
+            <h2>Macadamia Black Tea Soda</h2>
+            <form>
+              <span className="opcion-radio">
+                <input type="radio" id={id_input} name="tipo_comida"  />
+                <label htmlFor={id_input}></label>
+              </span>
+            </form>
+          </div>
+          <hr />
+          <div className="details">
+            <h3>Chico</h3>
+            <h3>Regular</h3>
+            <h3>Menos Azucar</h3>
+            <h3>Sin tapa</h3>
+          </div>
+          <hr />
+          <div className="button">
+            <CompleteOrderButton />
+          </div>
         </div>
       </div>
-      <div className="order_info">
-        <div className="item_name">
-          <h2>Macadamia Black Tea Soda</h2>
-          <input
-            type="radio"
-            name="ready"
-            value="yes"
-          />
-        </div>
-
-        <hr />
-        <div className="details">
-          <h3>Chico</h3>
-          <h3>Regular</h3>
-          <h3>Menos Azucar</h3>
-          <h3>Sin tapa</h3>
-        </div>
-        <hr />
-        <div className="button">
-          <CompleteOrderButton />
-        </div>
-      </div>
-    </div>
     </div>
   );
-}
+};
 export default OrderCard;

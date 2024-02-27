6c6a0b81ee6741e122d53fbc90ab3055f851e5ee
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-02-27 13:41:43 -0500

    Update App Frond

--- src/components/CompleteOrderButton copy/CompleteOrderButton.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./CompleteOrderButton.scss";
-function CompleteOrderButton() {
-  return <button className="order-button">Completar Orden</button>;
-}
-export default CompleteOrderButton;

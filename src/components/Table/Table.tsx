8f90539e490880c1ba90f48ae215ee6438ef8132
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-02-26 19:06:43 -0500

    Update  Table

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,11 +1,7 @@
-import {
-  useReactTable,
-  getCoreRowModel,
-  flexRender,
-} from "@tanstack/react-table";
+
 import data from "./inventory.json";
-import "./Table.scss";
-import OrderCard from "../OrderCard/OrderCard";
+
+
 type Inventory = {
   id: string;
   name: string;
@@ -33,42 +29,11 @@ const columns = [
   },
 ];
 function Table() {
-  const table = useReactTable({
-    columns,
-    data,
-    getCoreRowModel: getCoreRowModel(),
-  });
-  console.log(table.getRowModel().rows);
+ 
 
   return (
     <div className="table">
-      <table>
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th key={header.id}>
-                  {flexRender(
-                    header.column.columnDef.header,
-                    header.getContext()
-                  )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    
     </div>
   );
 }

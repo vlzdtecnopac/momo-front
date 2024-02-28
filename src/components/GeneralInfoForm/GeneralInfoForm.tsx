6fef5fb28411df004ddf6e89ce8165e53d8dd912
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-02-28 18:44:05 -0500

    Update App

--- src/components/GeneralInfoForm/GeneralInfoForm.tsx
+++ src/components/GeneralInfoForm/GeneralInfoForm.tsx
@@ -2,55 +2,29 @@ import "./GeneralInfoForm.scss";
 function GeneralInfoForm() {
   return (
     <>
-      <form
-        className="info-form"
-        action=""
-        method="POST"
-      >
-<div className="grid-2_xs-1">
-    <div className="col">
-        
-    <div className="group">
-          <label htmlFor="store">Tienda</label>
-            <input
-              id="store"
-              type="text"
-              required
-            />
+      <form className="info-form" action="" method="POST">
+        <div className="grid-2_xs-2_sm-2">
+          <div className="col">
+            <div className="group">
+              <label htmlFor="store">Tienda</label>
+              <input id="store" type="text" required />
+            </div>
+            <div className="group">
+              <label htmlFor="email">Cierre anterior</label>
+              <input id="close" type="datetime-local" required />
+            </div>
           </div>
-          <div className="group">
-          <label htmlFor="email">Cierre anterior</label>
-            <input
-              id="close"
-              type="datetime-local"
-              required
-            />
+          <div className="col">
+            <div className="group">
+              <label htmlFor="email">Email</label>
+              <input id="email" type="text" required />
+            </div>
+            <div className="group">
+              <label htmlFor="email">Apertura</label>
+              <input id="open" type="datetime-local" required />
+            </div>
           </div>
-    </div>
-    <div className="col">
-    <div className="group">
-          <label htmlFor="email">Email</label>
-            <input
-              id="email"
-              type="text"
-              required
-            />
-          </div>
-          <div className="group">
-          <label htmlFor="email">Apertura</label>
-            <input
-              id="open"
-              type="datetime-local"
-              required
-            />
         </div>
-    </div>
-</div>
-        
-
- 
-
-          
       </form>
     </>
   );

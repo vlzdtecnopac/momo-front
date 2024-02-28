f9a61caacc88e8e93ba7803d155562e4c5fff899
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-02-28 15:38:42 -0500

    Update Error

--- src/pages/fullScreen/FullScreen.tsx
+++ src/pages/fullScreen/FullScreen.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import OrderCard, { State } from "../../components/OrderCard/OrderCard";

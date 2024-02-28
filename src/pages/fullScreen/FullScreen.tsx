28585b4e001a396adf95be76d909e727dfe62b7d
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-02-28 13:06:20 -0500

    Update Frond

--- src/pages/fullScreen/FullScreen.tsx
+++ src/pages/fullScreen/FullScreen.tsx
@@ -1,6 +1,39 @@
+import OrderCard, { State } from "../../components/OrderCard/OrderCard";
+import OrderCardTwo from "../../components/OrderCard/OrderCard copy";
+import "./FullScreen.scss";
+
 export default function FullScreen(){
     return(
         <>
+<div className="columns">
+  <article>
+   <OrderCard state={State.initial} />
+  </article>
+  <article>
+  <OrderCard state={State.initial} />
+  </article>
+  <article>
+  <OrderCardTwo state={State.completed} />
+  </article>
+  <article>
+  <OrderCard state={State.completed} />
+  </article>
+  <article>
+  <OrderCardTwo state={State.pending} />
+  </article>
+  <article>
+  <OrderCard state={State.initial} />
+  </article>
+  <article>
+  <OrderCard state={State.initial} />
+  </article>
+  <article>
+  <OrderCardTwo state={State.pending} />
+  </article>
+  <article>
+  <OrderCard state={State.completed} />
+  </article>
+</div>
         </>
     );
     }

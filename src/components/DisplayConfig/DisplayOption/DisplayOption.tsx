f42bb475608ed9aa68f9d40f21f754b5ff2065bd
miketpac <michaelgonzalez@tecnopac.com.co>
2024-02-28 18:38:42 -0500

    options icons added

--- src/components/DisplayConfig/DisplayOption/DisplayOption.tsx
+++ src/components/DisplayConfig/DisplayOption/DisplayOption.tsx
@@ -1,4 +1,15 @@
+import "./DisplayOption.scss";
+import split from "../../../assets/icons/split.svg";
 function DisplayOption() {
-  return <div className="d-option"></div>;
+  return (
+    <div className="display-option">
+      <img
+        className="option-icon"
+        src={split}
+        alt="icon"
+      />
+      <h3>Split</h3>
+    </div>
+  );
 }
 export default DisplayOption;

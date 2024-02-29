096ae3d6839be7d1d97673765b0ba0d4c4458fb6
miketpac <michaelgonzalez@tecnopac.com.co>
2024-02-29 14:32:02 -0500

    fix order tabs page folder

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,7 +1,7 @@
 import Header from "../../includes/Header/Header";
 import InfoSidebar from "../../components/InfoSidebar/InfoSidebar";
 import SideBar from "../../includes/SideBar/SideBar";
-import Tabs from "../../components/Tabs/OrderTabs";
+import Tabs from "../../components/Tabs/OrderTabs/OrderTabs";
 import Layout from "../../includes/layout/Layout";
 import "./DashboardPage.scss";
 

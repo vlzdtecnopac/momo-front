aedf7319ac2b5c23164d6eea80f4a687be25d116
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-02-28 19:24:08 -0500

    Update App

--- src/components/GeneralInfoForm/GeneralInfoForm.tsx
+++ src/components/GeneralInfoForm/GeneralInfoForm.tsx
@@ -2,26 +2,26 @@ import "./GeneralInfoForm.scss";
 function GeneralInfoForm() {
   return (
     <>
-    <h2>Infromación General</h2>
+    <h2 className="info-general-title">Infromación General</h2>
       <form className="info-form" action="" method="POST">
         <div className="grid-3_xs-2_sm-2">
           <div className="col-5">
             <div className="group">
               <label htmlFor="store">Tienda</label>
-              <input id="store" type="text" required />
+              <input id="store" placeholder="Tienda 1" type="text" required />
             </div>
             <div className="group">
-              <label htmlFor="email">Cierre anterior</label>
+              <label htmlFor="close">Cierre anterior</label>
               <input id="close" type="datetime-local" required />
             </div>
           </div>
           <div className="col-5">
             <div className="group">
               <label htmlFor="email">Email</label>
-              <input id="email" type="text" required />
+              <input id="email" placeholder="mitienda@gmail.com" type="text" required />
             </div>
             <div className="group">
-              <label htmlFor="email">Apertura</label>
+              <label htmlFor="open">Apertura</label>
               <input id="open" type="datetime-local" required />
             </div>
           </div>

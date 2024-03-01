b751e66aa3e46ccd0b74171b7415bc8ab5e224f9
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-01 13:37:44 -0500

    Update App

--- src/components/DisplayConfig/DisplayConfig.tsx
+++ src/components/DisplayConfig/DisplayConfig.tsx
@@ -11,54 +11,54 @@ const options = [
 ];
 
 function DisplayConfig() {
- 
   return (
     <>
-    <div className="config-display-section">
-      <h2 className="title">Configuración display</h2>
-      <p className="sub-title">Visualización</p>
-    
-        <div className="grid-3_xs-1">
+      <div className="config-display-section">
+        <h2 className="title">Configuración display</h2>
+        <p className="sub-title">Visualización</p>
+
+        <div className="grid-3_xs-12">
           <div className="col-4">
-          <div className="display-config">
-            {options.map((option) => (
-              <DisplayOption
-                key={option.text}
-                icon={option.icon}
-                text={option.text}
-              />
-            ))}
+            <div className="display-config">
+              {options.map((option) => (
+                <DisplayOption
+                  key={option.text}
+                  icon={option.icon}
+                  text={option.text}
+                />
+              ))}
             </div>
           </div>
-          <div className="col-8-center">
+          <div className="col-8">
             <div className="column-option-text">
               <div className="radio-group">
                 <div className="opcion-radio-text">
                   <input type="radio" id="small-text" name="check_text" />
-                  <label htmlFor="small-text" ></label>
+                  <label htmlFor="small-text"></label>
                 </div>
               </div>
               <div className="radio-group">
                 <div className="opcion-radio-text">
                   <input type="radio" id="medium-text" name="check_text" />
-                  <label htmlFor="medium-text" ></label>
+                  <label htmlFor="medium-text"></label>
                 </div>
               </div>
               <div>
                 <div className="opcion-radio-text">
                   <input type="radio" id="large-text" name="check_text" />
-                  <label htmlFor="large-text" ></label>
+                  <label htmlFor="large-text"></label>
                 </div>
               </div>
             </div>
+            <div>
+            <div className="btns">
+              <button className="btn back">Regresar</button>
+              <button className="btn save">Guardar cambios</button>
+            </div>
+          </div>
           </div>
         </div>
-        <div className="btns">
-          <button className="btn back">Regresar</button>
-          <button className="btn save">Guardar cambios</button>
-        </div>
       </div>
-      
     </>
   );
 }

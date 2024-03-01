74bab3f374aae49c7a9e2a77dd020c0b879a6db1
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-01 18:20:49 -0500

    Update App

--- src/components/DisplayConfig/DisplayConfig.tsx
+++ src/components/DisplayConfig/DisplayConfig.tsx
@@ -1,9 +1,12 @@
 import "./DisplayConfig.scss";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import DisplayOption from "./DisplayOption/DisplayOption";
 import splitIcon from "../../assets/icons/split.svg";
 import stackedIcon from "../../assets/icons/stacked.svg";
 import clasicIcon from "../../assets/icons/clasic.svg";
-import { useState } from "react";
+
 
 const options = [
   { icon: splitIcon, text: "Split" },
@@ -12,6 +15,7 @@ const options = [
 ];
 
 function DisplayConfig() {
+  const navigate = useNavigate();
   const [stateText, setStateText] = useState<string>("medium");
   const [stateColumn, setStateColumn] = useState<number>(0);
 
@@ -26,6 +30,10 @@ function DisplayConfig() {
   const saveChangeHandler = () => {
     
 
+  }
+
+  const handleBackNavegation = () => {
+    navigate("/dashboard")
   }
 
   return (
@@ -89,7 +97,7 @@ function DisplayConfig() {
             </div>
             <div className="column-save-options">
               <div className="btns">
-                <button className="btn back">Regresar</button>
+                <button onClick={()=>handleBackNavegation()} className="btn back">Regresar</button>
                 <button  onClick={()=>saveChangeHandler()} className="btn save">Guardar cambios</button>
               </div>
             </div>

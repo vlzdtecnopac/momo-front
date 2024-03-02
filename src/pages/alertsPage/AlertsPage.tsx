b149383bb828c0633820c42358982f3bed992eb9
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-02 17:36:28 -0500

    Update App

--- src/pages/alertsPage/AlertsPage.tsx
+++ src/pages/alertsPage/AlertsPage.tsx
@@ -2,6 +2,7 @@ import AlertsTable from "../../components/Table/alertsTable";
 import Layout from "../../includes/layout/Layout";
 import "./AlertsPage.scss";
 import "./../dashboard/DashboardPage.scss";
+
 function AlertsPage() {
   
   return (

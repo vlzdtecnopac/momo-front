f0497ef9d2e3298a57e843a03b7e18e3b989ea29
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-02 08:27:26 -0500

    Update App Full Screen

--- src/pages/fullScreen/FullScreen.tsx
+++ src/pages/fullScreen/FullScreen.tsx
@@ -2,9 +2,12 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import OrderCard, { State } from "../../components/OrderCard/OrderCard";
 import OrderCardTwo from "../../components/OrderCard/OrderCard copy";
+import { useDesignStore } from "../../store/design.store";
 
 export default function FullScreen() {
+  const {typeTypography, selectTypography, typeColumns, selectTypeColumn} =  useDesignStore();
   const navigate = useNavigate();
+
   const handleEscapeKey = (event: KeyboardEvent) => {
     if (event.key === "Escape") {
       navigate("/dashboard");
@@ -20,36 +23,58 @@ export default function FullScreen() {
   }, []);
 
   return (
-    <>
-      <div className="columns-stacked">
-        <article>
-          <OrderCard state={State.initial} />
-        </article>
-        <article>
-          <OrderCard state={State.initial} />
-        </article>
-        <article>
-          <OrderCardTwo state={State.completed} />
-        </article>
-        <article>
-          <OrderCard state={State.completed} />
-        </article>
-        <article>
-          <OrderCardTwo state={State.pending} />
-        </article>
-        <article>
-          <OrderCard state={State.initial} />
-        </article>
-        <article>
-          <OrderCard state={State.initial} />
-        </article>
-        <article>
-          <OrderCardTwo state={State.pending} />
-        </article>
-        <article>
-          <OrderCard state={State.completed} />
-        </article>
-      </div>
-    </>
+    <div className="full_screen_page">
+ {(() => {
+        if(typeColumns == 1) {
+          return(<div className="columns-stacked">
+          <article>
+            <OrderCard state={State.initial} />
+          </article>
+          <article>
+            <OrderCard state={State.initial} />
+          </article>
+          <article>
+            <OrderCardTwo state={State.completed} />
+          </article>
+          <article>
+            <OrderCard state={State.completed} />
+          </article>
+          <article>
+            <OrderCardTwo state={State.pending} />
+          </article>
+          <article>
+            <OrderCard state={State.initial} />
+          </article>
+          <article>
+            <OrderCard state={State.initial} />
+          </article>
+          <article>
+            <OrderCardTwo state={State.pending} />
+          </article>
+          <article>
+            <OrderCard state={State.completed} />
+          </article>
+        </div>);
+        }else{
+          return(<div className={`${typeColumns == 0 ? 'grid-equalHeight' :  ''} grid-4_lg-4_md-3_sm-1_xs-1`}>
+          <div className="col">
+            <OrderCard state={State.initial} />
+          </div>
+          <div className="col">
+            <OrderCard state={State.pending} />
+          </div>
+          <div className="col">
+            <OrderCardTwo state={State.completed} />
+          </div>
+          <div className="col">
+            <OrderCard state={State.completed} />
+          </div>
+          <div className="col">
+            <OrderCard state={State.completed} />
+          </div>
+        </div>)
+        }
+      })()}
+    </div>
   );
 }

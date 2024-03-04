d899d87ac325e4db7e81c391f0c59345c84490e2
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-04 18:58:34 -0500

    Update App

--- src/includes/layout/Layout.tsx
+++ src/includes/layout/Layout.tsx
@@ -11,13 +11,13 @@ const Layout: React.FC<DynamicLayoutProps> = (props) => {
   return (
     <>
       <Header />
-      <div className="grid-2">
+      <div className="grid-2 max-content">
         <div className="col-1_sm-1_md-1_lg-1 padding">
           <div className="sidebar margin">
             <SideBar />
           </div>
         </div>
-        <div className="col-11_sm-11_md_11_lg-11 pr-38">
+        <div className="col-11_sm-11_md-11_lg-11 padding-content">
           {props.children}
           <InfoSidebar />
         </div>

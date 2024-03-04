0e92e7cb96931dad22610e13708dec716e49c6f2
miketpac <michaelgonzalez@tecnopac.com.co>
2024-03-04 18:29:39 -0500

    update

--- src/includes/layout/Layout.tsx
+++ src/includes/layout/Layout.tsx
@@ -17,7 +17,7 @@ const Layout: React.FC<DynamicLayoutProps> = (props) => {
             <SideBar />
           </div>
         </div>
-        <div className="col-11_sm-11_md_11_lg-11">
+        <div className="col-11_sm-11_md_11_lg-11 pr-38">
           {props.children}
           <InfoSidebar />
         </div>

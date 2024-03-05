3166ae5ace20916bddf793329db3e87bd8095614
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-04 19:46:07 -0500

    Update Front

--- src/pages/fullScreen/FullScreen.tsx
+++ src/pages/fullScreen/FullScreen.tsx
@@ -4,6 +4,7 @@ import OrderCard, { State } from "../../components/OrderCard/OrderCard";
 import OrderCardTwo from "../../components/OrderCard/OrderCard copy";
 import { useDesignStore } from "../../store/design.store";
 import "./FullScreen.scss";
+import { Link } from "react-router-dom";
 
 export default function FullScreen() {
   const {typeTypography, selectTypography, typeColumns, selectTypeColumn} =  useDesignStore();
@@ -76,6 +77,7 @@ export default function FullScreen() {
         </div>)
         }
       })()}
+      <Link to="../dashboard"><i className="icon_close_full_screen"></i></Link>
     </div>
   );
 }

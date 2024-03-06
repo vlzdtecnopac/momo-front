c489bd86b929f8dd2024661f84a02525f86214e6
miketpac <michaelgonzalez@tecnopac.com.co>
2024-03-06 15:26:06 -0500

    buttons font sizes fixed

--- src/includes/SideBar/SideBar.tsx
+++ src/includes/SideBar/SideBar.tsx
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import "./SideBar.scss";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function SideBar() {
   const [selectedOption, setSelectedOption] = useState("");
+  const location = useLocation();
 
+  console.log(location);
   const handleOptionClick = (option: string) => {
     setSelectedOption(option);
   };
 
   const sidebarListNav = [
-    { icon: "orders-icon", name: "orders", router: "../dashboard" },
-    { icon: "inventory-icon", name: "inventory", router: "../inventory-page" },
-    { icon: "alert-icon", name: "alert", router: "../alerts-page" },
-    { icon: "config-icon", name: "config", router: "../config-page" },
+    { icon: "orders-icon", name: "orders", router: "/dashboard" },
+    { icon: "inventory-icon", name: "inventory", router: "/inventory-page" },
+    { icon: "alert-icon", name: "alert", router: "/alerts-page" },
+    { icon: "config-icon", name: "config", router: "/config-page" },
   ];
 
   return (
@@ -21,12 +23,18 @@ function SideBar() {
       <ul className="options__menu">
         {sidebarListNav.map((item, index) => (
           <li
-            className="option"
+            className={`option ${
+              location.pathname == item.router ? "active" : ""
+            }`}
             onClick={() => handleOptionClick(item.name)}
             key={index}
           >
             <Link to={item.router}>
-              <i className={`icon ${item.icon}`}></i>
+              <i
+                className={`icon ${
+                  location.pathname === item.router ? "active" : ""
+                } ${item.icon}`}
+              ></i>
             </Link>
           </li>
         ))}

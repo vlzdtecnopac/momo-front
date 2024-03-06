95726cea43457ef5dcab660c047360215835e8e6
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-06 11:44:49 -0500

    Update Front

--- src/components/InfoSidebar/InfoSidebar.tsx
+++ src/components/InfoSidebar/InfoSidebar.tsx
@@ -1,13 +1,41 @@
-import { Link } from "react-router-dom";
+import { Link, Location } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 import "./InfoSidebar.scss";
+import { alertPageRuta, configPageRuta } from "../../routes/Router";
+
 function InfoSidebar() {
-  return (<div className="info-sidebar-component">
-    <Link to="/full-alerts">
-      <i className="icon icon-info"></i>
-    </Link>
-    <Link to="/full-screen">
-      <i className="icon icon-fullscreen"></i>
-    </Link>
-  </div>);
+  const location: Location<any> = useLocation();
+  console.log('Ruta actual:', location.pathname);
+
+
+
+  return (
+  <>
+   {(() => {
+        if (location.pathname == alertPageRuta) {
+            return (
+              <div className="info-sidebar-component">
+              <Link to="/full-screen">
+                <i className="icon icon-fullscreen"></i>
+              </Link>
+            </div>
+            )
+        }else if(location.pathname == configPageRuta){
+          return null;
+        }else{
+          return (
+            <div className="info-sidebar-component">
+            <Link to="/full-alerts">
+              <i className="icon icon-info"></i>
+            </Link>
+            <Link to="/full-screen">
+              <i className="icon icon-fullscreen"></i>
+            </Link>
+          </div>
+          )
+        }
+      })()}
+  </>
+);
 }
 export default InfoSidebar;

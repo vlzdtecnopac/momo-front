6231da6b031c5ae523629f3f8bfff9864f9ccf27
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-12 20:47:08 -0500

    Update Front

--- src/pages/welcome/WelcomePage.tsx
+++ src/pages/welcome/WelcomePage.tsx
@@ -19,90 +19,89 @@ function WelcomePage() {
   const [loader, setLoader] = useState<Boolean>();
 
   const { socket } = useContext(SocketContext);
- 
+
   useEffect(() => {
     setLoader(true);
-    return () => {
-      fetchData(localStorage.getItem("store-momo")!).then((resp)=>{
-        if(resp){
-          socket.emit("kiosko-socket", {
-            shopping_id: localStorage.getItem("store-momo")!,
-          });
-          setLoader(false);
-        }
-      });
-    };
+    fetchData(localStorage.getItem("store-momo")!).then((resp) => {
+      if (resp) {
+        socket.emit("kiosko-socket", {
+          shopping_id: localStorage.getItem("store-momo")!,
+        });
+        setLoader(false);
+      }
+    });
   }, []);
 
   useEffect(() => {
     socket.on("kiosko-socket", (data: KioskoInterface[]) => setKioskos(data));
   }, [socket]);
 
-  return (<>
-    {loader ? <LoaderPage/> : ""}
-    <div className="component-welcome">
-      <div className="logo-container">
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            duration: 0.3,
-            ease: [0, 0.71, 0.2, 1.01],
-            scale: {
-              type: "spring",
-              damping: 5,
-              stiffness: 100,
-              restDelta: 0.001,
-            },
-          }}
-        >
-          <img className="logo" src={logo} alt="momo-logo" />
-        </motion.div>
-      </div>
-      <div className="text-container">
-        <div className="text">
-          <h2 className={`big-text ${typeTypography}-text`}>¡Bienvenid@!</h2>
-          <p
-            className={`sub-text ${typeTypography}-text`}
-            style={{ marginTop: "15px" }}
+  return (
+    <>
+      {loader ? <LoaderPage /> : ""}
+      <div className="component-welcome">
+        <div className="logo-container">
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.3,
+              ease: [0, 0.71, 0.2, 1.01],
+              scale: {
+                type: "spring",
+                damping: 5,
+                stiffness: 100,
+                restDelta: 0.001,
+              },
+            }}
           >
-            Antes de comenzar, <br />
-            Espera que se emparejen <br />
-            tus dispositivos.
-          </p>
+            <img className="logo" src={logo} alt="momo-logo" />
+          </motion.div>
         </div>
-      </div>
-      <div className="kds-loader-container">
-        <div className="kds-loader">
-          <div className="store-card">
-            {data?.map((item: any, i: number) => (
-              <Card
-                key={i}
-                icon={tabletIcon}
-                text={item.name_shopping}
-                subText={`No. ${item.no_shooping}`}
-                state={true}
-              />
-            ))}
+        <div className="text-container">
+          <div className="text">
+            <h2 className={`big-text ${typeTypography}-text`}>¡Bienvenid@!</h2>
+            <p
+              className={`sub-text ${typeTypography}-text`}
+              style={{ marginTop: "15px" }}
+            >
+              Antes de comenzar, <br />
+              Espera que se emparejen <br />
+              tus dispositivos.
+            </p>
           </div>
-          <div className="loader"></div>
-          <div className="card-group">
-            {kioskos?.map((_, index: number) => {
-              index = kioskos.length - 1 - index;
-              return (
+        </div>
+        <div className="kds-loader-container">
+          <div className="kds-loader">
+            <div className="store-card">
+              {data?.map((item: any, i: number) => (
                 <Card
-                  key={kioskos[index].id}
-                  icon={kioskIcon}
-                  text={kioskos[index].nombre}
-                  subText={kioskos[index].name_shopping}
-                  state={kioskos[index].state}
+                  key={i}
+                  icon={tabletIcon}
+                  text={item.name_shopping}
+                  subText={`No. ${item.no_shooping}`}
+                  state={true}
                 />
-              );
-            })}
+              ))}
+            </div>
+            <div className="loader"></div>
+            <div className="card-group">
+              {kioskos?.map((_, index: number) => {
+                index = kioskos.length - 1 - index;
+                return (
+                  <Card
+                    key={kioskos[index].id}
+                    icon={kioskIcon}
+                    text={kioskos[index].nombre}
+                    subText={kioskos[index].name_shopping}
+                    state={kioskos[index].state}
+                  />
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }

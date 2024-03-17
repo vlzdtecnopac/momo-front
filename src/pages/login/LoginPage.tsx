f48bae08b2a3f8629a12b88538902d4d1d0b29e9
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-17 10:43:31 -0500

    Update Frontend

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,7 +1,13 @@
 
 import imgLogin from "./../../assets/logo.svg";
 import Login from "../../components/Login/Login";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 const LoginPage = () => {
+  const navigate = useNavigate();
+  useEffect(()=> {
+    navigate("/dashboard");
+  }, [])
   return (
     <div>
       <Login img={imgLogin} />
